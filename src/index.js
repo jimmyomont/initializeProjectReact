f2f847d8500961259d87f59e18f51789620a608b
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-05-29 11:19:01 +0200

    README

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-    
-    import   App  from "./components/App";
-    
-    const domElement = document.getElementById('app');
-    const root = createRoot(domElement);
-    root.render(<App />);
